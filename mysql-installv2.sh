d156e4a0ccc17cc348c976dcb0ecdd9d707817e2
ionoslabs <seth@ionoslabs.com>
2022-03-17 13:09:42 -0400

    mysql-installv2 script added

---
+++ mysql-installv2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Creating MySQL user and database"
+
+/usr/bin/mysql -u root -e "CREATE DATABASE $db_name"
+/usr/bin/mysql -u root -e "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password';"
+/usr/bin/mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';"
+
+echo "done"
+
+
+
+# secure Mysql installation
+
+echo "changing bind address"
+
+sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+service mysql restart
