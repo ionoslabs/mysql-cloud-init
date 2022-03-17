9d83399cf8e53501c7466ccc16120c40a03b644f
ionoslabs <seth@ionoslabs.com>
2022-03-17 12:40:12 -0400

    removed secure install script for testing

--- mysql-install.sh
+++ mysql-install.sh
@@ -14,46 +14,46 @@ echo debconf mysql-server/root_password password $MYSQL_ROOT_PASSWORD | sudo deb
 echo debconf mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD | sudo debconf-set-selections
 apt-get -qq install mysql-server > /dev/null # Install MySQL quietly
 
-# Install Expect
-apt-get -qq install expect > /dev/null
+# # Install Expect
+# apt-get -qq install expect > /dev/null
 
-# Build Expect script
-tee ~/secure_our_mysql.sh > /dev/null << EOF
-spawn $(which mysql_secure_installation)
+# # Build Expect script
+# tee ~/secure_our_mysql.sh > /dev/null << EOF
+# spawn $(which mysql_secure_installation)
 
-expect "Enter password for user root:"
-send "$MYSQL_ROOT_PASSWORD\r"
+# expect "Enter password for user root:"
+# send "$MYSQL_ROOT_PASSWORD\r"
 
-expect "Press y|Y for Yes, any other key for No:"
-send "y\r"
+# expect "Press y|Y for Yes, any other key for No:"
+# send "y\r"
 
-expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
-send "2\r"
+# expect "Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:"
+# send "2\r"
 
-expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
-send "n\r"
+# expect "Change the password for root ? ((Press y|Y for Yes, any other key for No) :"
+# send "n\r"
 
-expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
+# expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+# send "y\r"
 
-expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
+# expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+# send "y\r"
 
-expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
+# expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+# send "y\r"
 
-expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
-send "y\r"
+# expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+# send "y\r"
 
-EOF
+# EOF
 
-# Run Expect script.
-# This runs the "mysql_secure_installation" script which removes insecure defaults.
-expect ~/secure_our_mysql.sh
+# # Run Expect script.
+# # This runs the "mysql_secure_installation" script which removes insecure defaults.
+# expect ~/secure_our_mysql.sh
 
-# Cleanup
-rm -v ~/secure_our_mysql.sh # Remove the generated Expect script
-#sudo apt-get -qq purge expect > /dev/null # Uninstall Expect, commented out in case you need Expect
+# # Cleanup
+# rm -v ~/secure_our_mysql.sh # Remove the generated Expect script
+# #sudo apt-get -qq purge expect > /dev/null # Uninstall Expect, commented out in case you need Expect
 
 echo "Binding MYSQL to all interfaces"
 
@@ -75,6 +75,3 @@ echo "MySQL setup completed. Insecure defaults are gone."
 
 echo "Your MYSQL root password has been set to="$MYSQL_ROOT_PASSWORD
 
-# create wpuser
-# create random password for wpuser
-# grant access to wpuser from IP address
